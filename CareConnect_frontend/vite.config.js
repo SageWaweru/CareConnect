2ded1ce197e2784c99b6c6c46f3b0379632d7286
SageWaweru <sagewaweru2005@gmail.com>
2025-02-19 18:14:43 +0300

    static

--- CareConnect_frontend/vite.config.js
+++ CareConnect_frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/static/",  // Change Vite's output path to use Django's static URL
+  build: {
+    outDir: "../care_app/static/",  // Ensure it outputs to Django's static directory
+    emptyOutDir: true, // Clears old files on rebuild
+    manifest: true, // Generates manifest.json for Django to use
+  }
+});
