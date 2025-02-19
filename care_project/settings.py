3917c4a628625de3d855bcb84bdfb30c73972924
SageWaweru <sagewaweru2005@gmail.com>
2025-02-19 18:37:06 +0300

    static

--- care_project/settings.py
+++ care_project/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 STATIC_URL = '/static/'   
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "CareConnect_frontend", "dist", "assets"),
+    os.path.join(BASE_DIR, "CareConnect_frontend", "dist", "static"),
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
